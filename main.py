c68299669801604c0ba7a79fd9e988097a73316d
KigoryGH <alialmowali213@gmail.com>
2026-08-26 16:57:01 +0300

    bug fixes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from textual.app import App
-from textual.containers import Horizontal
+from textual.containers import Horizontal, Vertical
 from textual.widgets import Static
 
 class AptListApp(App):
@@ -8,7 +8,9 @@ class AptListApp(App):
     def compose(self):
         yield Static("aptlist", id="title")
         with Horizontal():
-            yield Static("Search", id="sidebar")
+            with Vertical(id="sidebar"):
+                with Vertical(id="search"):
+                    yield Static("Search")
             yield Static("Information", id="main")
 
 if __name__ == "__main__":
